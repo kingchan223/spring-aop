0205ba54e28e47c0d3b4433782a9eb81d5a2d1a6
kingchan223 <kokiyo97@naver.com>
2022-01-14 19:07:53 +0900

    어드바이스 종류

--- src/main/java/hello/aop/order/OrderService.java
+++ src/main/java/hello/aop/order/OrderService.java
@@ -11,8 +11,8 @@ public class OrderService {
     public OrderService(OrderRepository orderRepository) {
         this.orderRepository = orderRepository;
     }
-    public void orderItem(String itemId) {
+    public String orderItem(String itemId) {
         log.info("[orderService] 실행");
-        orderRepository.save(itemId);
+        return orderRepository.save(itemId);
     }
 }
--- src/test/java/hello/aop/AopTest.java
+++ src/test/java/hello/aop/AopTest.java
@@ -3,6 +3,7 @@ package hello.aop;
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
 import hello.aop.order.aop.AspectV5Order;
+import hello.aop.order.aop.AspectV6Advice;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -12,7 +13,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
 //임포트를 따로 해줘야 한다.
-@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class})
+//@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class})
+@Import({AspectV6Advice.class})
 @Slf4j
 @SpringBootTest
 public class AopTest {
@@ -31,7 +33,7 @@ public class AopTest {
 
     @Test
     void success(){
-        orderService.orderItem("itemA");
+        orderService.orderItem("hello");//테스트를 위해 익셉션을 발생시키는 itemId
     }
 
 
